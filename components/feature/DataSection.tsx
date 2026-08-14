0ec169953e59629cb2f107ea34e8e0d7e9025a9c
nishimura-yuma77 <nishimura.ym0707@gmail.com>
2026-08-14 15:22:02 +0900

    datasectionの完成

---
+++ components/feature/DataSection.tsx
@@ -0,0 +1,52 @@
+"use client"
+import useIntersectionObserver from "@/hooks/useIntersectionObserver"
+import SectionContainer from "@/components/primitives/SectionContainer"
+import SectionTitle from "@/components/primitives/SectionTitle"
+import style from "@/styles/feature/DataSection.module.scss"
+import TypewriterText from "../primitives/TypewriterText"
+
+const PROFILE_DATA = [
+  {
+    header: "BIRTHDAY",
+    content: "7月7日"
+  },
+  {
+    header: "HEIGHT",
+    content: "167cm"
+  },
+  {
+    header: "LIKES",
+    content: "ゲーム / 開発"
+  },
+  {
+    header: "DISLIKES",
+    content: "野菜 / 片づけ"
+  },
+  {
+    header: "MOTTO",
+    content: "チビじゃない。器がデカい。"
+  }
+]
+export default function DataSection() {
+  const {
+    ref,
+    isVisible
+  } = useIntersectionObserver({threshold: 0})
+  return (
+    <SectionContainer ref={ref}>
+      <SectionTitle title={"003_DATA"} isVisible={isVisible} />
+      <table className={style.profile_table}>
+        <tbody>
+          {PROFILE_DATA.map((profile) => {
+            return (
+              <tr>
+                <th scope="row"><TypewriterText text={profile.header} isVisible={isVisible}/></th>
+                <td><TypewriterText text={profile.content} isVisible={isVisible}/></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </SectionContainer>
+  )
+}
