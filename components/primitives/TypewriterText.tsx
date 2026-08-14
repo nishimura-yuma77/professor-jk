01ad9321c5bce14cb4424cba746bcdb92b0d0222
nishimura-yuma77 <nishimura.ym0707@gmail.com>
2026-08-14 14:12:31 +0900

    TypewriterTextにアニメーション終了のイベントハンドラを追加

--- components/primitives/TypewriterText.tsx
+++ components/primitives/TypewriterText.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import style from "@/styles/primitives/TypewriterText.module.scss"
 
 type TypewriterTextProps = {
@@ -5,22 +7,26 @@ type TypewriterTextProps = {
   isVisible: boolean
   className?: string,
   animationDelay?: number // ms
+  onAnimationEnd?: () => void
 }
 
 export default function TypewriterText({
   text,
   isVisible,
   className,
-  animationDelay = 80
+  animationDelay = 80,
+  onAnimationEnd
 }: TypewriterTextProps) {
   return (
     <span className={style.text}>
       {text.split("").map((char, index) => {
+        const isLast = index === text.length - 1
         return (
           <span
             key={index}
             style={{animationDelay: `${animationDelay * index}ms`}}
             className={`${style.char} ${isVisible ? style.char_active : ""} ${className ?? className}`}
+            onAnimationEnd={isLast ? () => onAnimationEnd : undefined}
           >{char}</span>
         )
       })}
