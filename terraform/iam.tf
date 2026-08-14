3ee09ab117e20a116be8144d7776ab5b15368b90
nishimura-yuma77 <nishimura.ym0707@gmail.com>
2026-08-14 21:42:41 +0900

    terraformでawsリソースを管理

---
+++ terraform/iam.tf
@@ -0,0 +1,90 @@
+// CodeBuildがこのRoleを引き受けることを許可する信頼ポリシー
+data "aws_iam_policy_document" "codebuild_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+// CodeBuild用Service Role
+resource "aws_iam_role" "codebuild" {
+  name = "professor-jk-codebuild-role"
+
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
+}
+
+data "aws_iam_policy_document" "codebuild_permissions" {
+
+  // S3 bucket自体への操作
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+
+    resources = [
+      aws_s3_bucket.site.arn
+    ]
+  }
+
+  // S3内のオブジェクト操作
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.site.arn}/*"
+    ]
+  }
+
+  // CloudFrontキャッシュ削除
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "cloudfront:CreateInvalidation"
+    ]
+
+    resources = [
+      aws_cloudfront_distribution.site.arn
+    ]
+  }
+
+  // CodeBuildのログ出力
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+// ロールを作成し、codebuildに割り当て
+resource "aws_iam_role_policy" "codebuild" {
+  name = "professor-jk-codebuild-policy"
+  role = aws_iam_role.codebuild.id
+
+  policy = data.aws_iam_policy_document.codebuild_permissions.json
+}
---
+++ terraform/main.tf
@@ -0,0 +1,110 @@
+// フロントソース配置用のs3バケット
+resource "aws_s3_bucket" "site" {
+  bucket_prefix = "professor-jk-site-" // 作成するバケットのprefix
+}
+
+// s3バケットへのpublicアクセスを制限する
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls = true // public ACLを作れない
+  block_public_policy = true // public policyを作らせない
+  ignore_public_acls = true // public ACLを利かない
+  restrict_public_buckets = true // public policyがあってもpublicアクセスを制限
+}
+
+// CloudFront Origin Access Control CloudFrontがprivate s3を読めるようにする
+resource "aws_cloudfront_origin_access_control" "site" {
+  name = "professor-jk-oac"
+  description = "Origin Access Control for professor-jk site"
+  origin_access_control_origin_type = "s3"
+  signing_behavior = "always" // CloudFront -> S3の署名を常に行う。CloudFront経由のリクエストであることを証明する
+  signing_protocol = "sigv4" // AWS　Signature Version 4での署名(現状署名方式はこれひとつのみ)
+}
+
+// CloudFrontのディストリビューション
+resource "aws_cloudfront_distribution" "site" {
+  enabled = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id = "professor-jk-s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.site.id
+  }
+
+  default_cache_behavior {
+    target_origin_id = "professor-jk-s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  // !! 存在しないURLへのアクセス時、S3が403を返す場合がある。これを拾い、index.htmlへリダイレクトする !!
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+    error_caching_min_ttl = 0
+  }
+}
+
+data "aws_iam_policy_document" "site_bucket_policy" {
+  statement {
+    sid = "AllowCloudFrontReadOnly"
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.site.arn}/*"
+    ]
+
+    // CloudFrontからのアクセスをすべて許可にはせず、このアプリのディストリビューションからのみのアクセスを許可する
+    condition {
+      test = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [
+        aws_cloudfront_distribution.site.arn
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.site_bucket_policy.json
+}
