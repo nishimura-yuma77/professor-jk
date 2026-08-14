29deab450453721f591324ccb5740a00619b4e4d
nishimura-yuma77 <nishimura.ym0707@gmail.com>
2026-08-14 17:33:00 +0900

    Headerをロゴのみに。初版はこれで

--- components/feature/Header.tsx
+++ components/feature/Header.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import style from "@/styles/feature/Header.module.scss"
 import Logo from "@/components/ui/Logo"
+import BurgerMenu from "@/components/feature/BurgerMenu"
 
 export default function Header() {
   return (
