f3952a3488bb44428f135497c8e9f6e520483825
nishimura-yuma77 <nishimura.ym0707@gmail.com>
2026-08-14 10:50:55 +0900

    active badgeの感性

---
+++ components/ui/ActiveBadge.tsx
@@ -0,0 +1,26 @@
+import style from "@/styles/ui/ActiveBadge.module.scss"
+import { useEffect, useState } from "react";
+
+type ActiveBadgeProps = {
+  isVisible: boolean
+  activeTransitionDelay?: number
+}
+
+export default function ActiveBadge({
+  isVisible,
+  activeTransitionDelay = 1000
+}: ActiveBadgeProps) {
+  const [isOnline, setIsOnline] = useState<boolean>(false);
+  useEffect(() => {
+    if (!isVisible) return
+    const timer = setTimeout(() => {
+      setIsOnline(true)
+    }, activeTransitionDelay)
+  }, [isVisible])
+  return (
+    <div className={style.container}>
+      <span className={isOnline ? style.green_dot : style.red_dot} />
+      <span className={style.text}>{isOnline ? "online" : "offline"}</span>
+    </div>
+  )
+}
