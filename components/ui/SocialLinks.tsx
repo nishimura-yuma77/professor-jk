117ab052a7ed07dd70bc6dce2be248bee7630993
nishimura-yuma77 <nishimura.ym0707@gmail.com>
2026-09-09 16:40:14 +0900

    chore: clear existing lint warnings

--- components/ui/SocialLinks.tsx
+++ components/ui/SocialLinks.tsx
@@ -1,6 +1,3 @@
-import { YOUTUBE_LINK, XTWITTER_LINK } from "@/const/constants"
-import XTwitterIcon from "@/components/primitives/XTwitterIcon"
-import YoutubeIcon from "@/components/primitives/YoutubeIcon"
 import style from "@/styles/ui/SocialLinks.module.scss"
 import XTwitterLinkIcon from "@/components/ui/XTwitterLinkIcon"
 import YoutubeLinkIcon from "@/components/ui/YoutubeLinkIcon"
